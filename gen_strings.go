2789b78728e9395071aa87ad8abcfd3cc4db5d88
Kevin McDonald <sudorandom@users.noreply.github.com>
2026-05-29 15:11:12 +0200

    Add more heuristics (#97)
    * feat: add several new heuristics
    
    * chore: update test
    
    * chore: more heuristics
    
    * chore: use faker from opts in bytes generator

--- gen_strings.go
+++ gen_strings.go
@@ -5,6 +5,7 @@ import (
 	"strconv"
 	"strings"
 	"sync"
+	"time"
 
 	"buf.build/gen/go/bufbuild/protovalidate/protocolbuffers/go/buf/validate"
 	"google.golang.org/protobuf/reflect/protoreflect"
@@ -38,6 +39,48 @@ func stringByHeuristics(fd protoreflect.FieldDescriptor, opts GenOptions) (strin
 	lowerName := strings.ToLower(string(fd.Name()))
 	var f heuristicFunc
 	switch {
+	case isTagOrAttributesKey(fd):
+		f = func(opts GenOptions) string { return strings.ToLower(opts.fake().Word()) }
+	case strings.Contains(lowerName, "id"):
+		f = func(opts GenOptions) string { return opts.fake().UUID() }
+	case strings.Contains(lowerName, "token"):
+		f = func(opts GenOptions) string { return opts.fake().UUID() }
+	case strings.Contains(lowerName, "email") || strings.Contains(lowerName, "mail"):
+		f = func(opts GenOptions) string { return strings.ToLower(opts.fake().Email()) }
+	case isPhoneField(lowerName):
+		f = func(opts GenOptions) string { return opts.fake().Phone() }
+	case isIPField(lowerName):
+		f = func(opts GenOptions) string { return opts.fake().IPv4Address() }
+	case isMacField(lowerName):
+		f = func(opts GenOptions) string { return opts.fake().MacAddress() }
+	case strings.Contains(lowerName, "user_agent") || lowerName == "ua":
+		f = func(opts GenOptions) string { return opts.fake().UserAgent() }
+	case strings.Contains(lowerName, "color") || strings.Contains(lowerName, "hex"):
+		f = func(opts GenOptions) string { return opts.fake().HexColor() }
+	case isTimeField(lowerName):
+		f = func(opts GenOptions) string { return opts.fake().Date().Format(time.RFC3339) }
+	case lowerName == "company" || strings.Contains(lowerName, "company_name") || lowerName == "organization" || lowerName == "org":
+		f = func(opts GenOptions) string { return opts.fake().Company() }
+	case lowerName == "job_title" || lowerName == "title" || strings.Contains(lowerName, "job_title"):
+		f = func(opts GenOptions) string { return opts.fake().JobTitle() }
+	case lowerName == "currency" || strings.Contains(lowerName, "currency_code"):
+		f = func(opts GenOptions) string { return opts.fake().CurrencyShort() }
+	case lowerName == "language" || lowerName == "lang" || strings.HasSuffix(lowerName, "_lang"):
+		f = func(opts GenOptions) string { return opts.fake().LanguageAbbreviation() }
+	case lowerName == "locale" || lowerName == "bcp47" || strings.HasSuffix(lowerName, "_locale"):
+		f = func(opts GenOptions) string { return opts.fake().LanguageBCP() }
+	case strings.Contains(lowerName, "address"):
+		f = func(opts GenOptions) string { return opts.fake().Address().Address }
+	case strings.Contains(lowerName, "street"):
+		f = func(opts GenOptions) string { return opts.fake().Address().Street }
+	case isCityField(lowerName):
+		f = func(opts GenOptions) string { return opts.fake().City() }
+	case strings.Contains(lowerName, "country"):
+		f = func(opts GenOptions) string { return opts.fake().Country() }
+	case strings.Contains(lowerName, "zip") || strings.Contains(lowerName, "postcode") || strings.Contains(lowerName, "postal"):
+		f = func(opts GenOptions) string { return opts.fake().Zip() }
+	case strings.Contains(lowerName, "description") || strings.Contains(lowerName, "bio") || strings.Contains(lowerName, "summary") || strings.Contains(lowerName, "comment") || strings.Contains(lowerName, "body") || strings.Contains(lowerName, "content"):
+		f = func(opts GenOptions) string { return opts.fake().Paragraph() }
 	case strings.Contains(lowerName, "name"):
 		if strings.Contains(lowerName, "firstname") {
 			f = func(opts GenOptions) string { return opts.fake().FirstName() }
@@ -48,10 +91,6 @@ func stringByHeuristics(fd protoreflect.FieldDescriptor, opts GenOptions) (strin
 		} else {
 			f = func(opts GenOptions) string { return opts.fake().FirstName() }
 		}
-	case strings.Contains(lowerName, "id"):
-		f = func(opts GenOptions) string { return opts.fake().UUID() }
-	case strings.Contains(lowerName, "token"):
-		f = func(opts GenOptions) string { return opts.fake().UUID() }
 	case strings.Contains(lowerName, "url"):
 		if strings.Contains(lowerName, "photo") {
 			f = func(opts GenOptions) string { return "https://picsum.photos/400" }
@@ -66,11 +105,10 @@ func stringByHeuristics(fd protoreflect.FieldDescriptor, opts GenOptions) (strin
 		}
 	}
 
+	heuristicCache.Store(fd, f)
 	if f == nil {
-		heuristicCache.Store(fd, nil)
 		return "", false
 	}
-	heuristicCache.Store(fd, f)
 	return f(opts), true
 }
 
@@ -213,6 +251,11 @@ func String(fd protoreflect.FieldDescriptor, opts GenOptions) string {
 			generatedString = generatedString[:maxLen]
 		}
 	}
+	if isTagOrAttributesKey(fd) {
+		if rules == nil || rules.Pattern == nil {
+			generatedString = strings.ToLower(generatedString)
+		}
+	}
 	return generatedString
 }
 
@@ -229,4 +272,83 @@ func generateHipsterText(minLen, maxLen uint64, opts GenOptions) string {
 		return b.String()[:maxLen]
 	}
 	return b.String()
+}
+
+func isTagOrAttributesKey(fd protoreflect.FieldDescriptor) bool {
+	lowerName := strings.ToLower(string(fd.Name()))
+	if strings.Contains(lowerName, "tag") {
+		return true
+	}
+	if fd.Name() != "key" || fd.Kind() != protoreflect.StringKind {
+		return false
+	}
+	md := fd.ContainingMessage()
+	if md == nil || !md.IsMapEntry() {
+		return false
+	}
+	parent := md.Parent()
+	if parent == nil {
+		return false
+	}
+	outerMsg, ok := parent.(protoreflect.MessageDescriptor)
+	if !ok {
+		return false
+	}
+	for i := range outerMsg.Fields().Len() {
+		field := outerMsg.Fields().Get(i)
+		if field.IsMap() && field.Message() == md {
+			lowerMapName := strings.ToLower(string(field.Name()))
+			return strings.Contains(lowerMapName, "attribute") || strings.Contains(lowerMapName, "attr")
+		}
+	}
+	return false
+}
+
+func isIPField(lowerName string) bool {
+	return lowerName == "ip" ||
+		strings.Contains(lowerName, "ip_address") ||
+		strings.HasPrefix(lowerName, "ip_") ||
+		strings.HasPrefix(lowerName, "ip-") ||
+		strings.HasSuffix(lowerName, "_ip") ||
+		strings.HasSuffix(lowerName, "-ip") ||
+		strings.Contains(lowerName, "_ip_") ||
+		strings.Contains(lowerName, "-ip-")
+}
+
+func isMacField(lowerName string) bool {
+	return lowerName == "mac" ||
+		strings.Contains(lowerName, "mac_address") ||
+		strings.HasPrefix(lowerName, "mac_") ||
+		strings.HasPrefix(lowerName, "mac-") ||
+		strings.HasSuffix(lowerName, "_mac") ||
+		strings.HasSuffix(lowerName, "-mac") ||
+		strings.Contains(lowerName, "_mac_") ||
+		strings.Contains(lowerName, "-mac-")
+}
+
+func isCityField(lowerName string) bool {
+	return lowerName == "city" ||
+		strings.HasPrefix(lowerName, "city_") ||
+		strings.HasPrefix(lowerName, "city-") ||
+		strings.HasSuffix(lowerName, "_city") ||
+		strings.HasSuffix(lowerName, "-city")
+}
+
+func isPhoneField(lowerName string) bool {
+	return strings.Contains(lowerName, "phone") ||
+		strings.Contains(lowerName, "mobile") ||
+		strings.Contains(lowerName, "fax") ||
+		lowerName == "tel" ||
+		strings.HasPrefix(lowerName, "tel_") ||
+		strings.HasPrefix(lowerName, "tel-") ||
+		strings.HasSuffix(lowerName, "_tel") ||
+		strings.HasSuffix(lowerName, "-tel")
+}
+
+func isTimeField(lowerName string) bool {
+	return strings.HasSuffix(lowerName, "_at") ||
+		lowerName == "date" ||
+		strings.HasSuffix(lowerName, "_date") ||
+		lowerName == "timestamp" ||
+		strings.HasSuffix(lowerName, "_timestamp")
 }
--- gen_strings_test.go
+++ gen_strings_test.go
@@ -2,7 +2,9 @@ package fauxrpc_test
 
 import (
 	"regexp"
+	"strings"
 	"testing"
+	"time"
 
 	"buf.build/gen/go/bufbuild/protovalidate/protocolbuffers/go/buf/validate"
 	"github.com/brianvoe/gofakeit/v7"
@@ -10,6 +12,7 @@ import (
 	"github.com/stretchr/testify/require"
 	"github.com/sudorandom/fauxrpc"
 	"google.golang.org/protobuf/reflect/protoreflect"
+	"google.golang.org/protobuf/types/dynamicpb"
 
 	testv1 "github.com/sudorandom/fauxrpc/private/gen/test/v1"
 )
@@ -181,5 +184,139 @@ func TestString(t *testing.T) {
 		s := fauxrpc.String(fd, opts)
 		assert.GreaterOrEqual(t, len([]byte(s)), int(minBytes))
 		assert.LessOrEqual(t, len([]byte(s)), int(maxBytes))
+	})
+
+	t.Run("tag/tags heuristics", func(t *testing.T) {
+		md := testv1.File_test_v1_test_proto.Messages().ByName("AllTypes")
+		opts := fauxrpc.GenOptions{Faker: gofakeit.New(0), MaxDepth: 2}
+
+		for _, name := range []string{"tag", "custom_tag"} {
+			fd := md.Fields().ByName(protoreflect.Name(name))
+			require.NotNil(t, fd, "field %s should exist", name)
+			s := fauxrpc.String(fd, opts)
+			assert.NotEmpty(t, s)
+			assert.Equal(t, strings.ToLower(s), s)
+			assert.NotContains(t, s, " ")
+		}
+
+		tagsFd := md.Fields().ByName("tags")
+		require.NotNil(t, tagsFd)
+		msg := dynamicpb.NewMessage(md)
+		// We want at least one item to test, so we retry if it generates 0 items
+		var val *protoreflect.Value
+		for range 10 {
+			val = fauxrpc.Repeated(msg.ProtoReflect(), tagsFd, opts)
+			if val != nil && val.List().Len() > 0 {
+				break
+			}
+		}
+		require.NotNil(t, val)
+		list := val.List()
+		assert.Greater(t, list.Len(), 0)
+		for i := 0; i < list.Len(); i++ {
+			s := list.Get(i).String()
+			assert.NotEmpty(t, s)
+			assert.Equal(t, strings.ToLower(s), s)
+			assert.NotContains(t, s, " ")
+		}
+	})
+
+	t.Run("expanded field name heuristics", func(t *testing.T) {
+		md := testv1.File_test_v1_test_proto.Messages().ByName("AllTypes")
+		require.NotNil(t, md)
+		opts := fauxrpc.GenOptions{Faker: gofakeit.New(0)}
+
+		tests := []struct {
+			name      string
+			fieldName string
+			validate  func(t *testing.T, val string)
+		}{
+			{"email", "email", func(t *testing.T, val string) {
+				assert.Contains(t, val, "@")
+				assert.Equal(t, strings.ToLower(val), val)
+			}},
+			{"phone", "phone", func(t *testing.T, val string) {
+				assert.NotEmpty(t, val)
+			}},
+			{"client_ip", "client_ip", func(t *testing.T, val string) {
+				assert.Regexp(t, regexp.MustCompile(`^\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}$`), val)
+			}},
+			{"device_mac", "device_mac", func(t *testing.T, val string) {
+				assert.Regexp(t, regexp.MustCompile(`^([0-9a-fA-F]{2}:){5}[0-9a-fA-F]{2}$`), val)
+			}},
+			{"user_agent", "user_agent", func(t *testing.T, val string) {
+				assert.NotEmpty(t, val)
+			}},
+			{"favorite_color", "favorite_color", func(t *testing.T, val string) {
+				assert.Regexp(t, regexp.MustCompile(`^#[0-9a-fA-F]{6}$`), val)
+			}},
+			{"home_address", "home_address", func(t *testing.T, val string) {
+				assert.NotEmpty(t, val)
+			}},
+			{"city", "city", func(t *testing.T, val string) {
+				assert.NotEmpty(t, val)
+			}},
+			{"country", "country", func(t *testing.T, val string) {
+				assert.NotEmpty(t, val)
+			}},
+			{"postal_code", "postal_code", func(t *testing.T, val string) {
+				assert.NotEmpty(t, val)
+			}},
+			{"profile_bio", "profile_bio", func(t *testing.T, val string) {
+				assert.Greater(t, len(strings.Split(val, " ")), 5)
+			}},
+		}
+
+		for _, tc := range tests {
+			t.Run(tc.name, func(t *testing.T) {
+				fd := md.Fields().ByName(protoreflect.Name(tc.fieldName))
+				require.NotNil(t, fd, "field %s should exist", tc.fieldName)
+				s := fauxrpc.String(fd, opts)
+				tc.validate(t, s)
+			})
+		}
+	})
+
+	t.Run("additional heuristics", func(t *testing.T) {
+		opts := fauxrpc.GenOptions{Faker: gofakeit.New(0)}
+		tests := []struct {
+			name     string
+			validate func(t *testing.T, val string)
+		}{
+			{"created_at", func(t *testing.T, val string) {
+				_, err := time.Parse(time.RFC3339, val)
+				assert.NoError(t, err, "value %q should be RFC3339 timestamp", val)
+			}},
+			{"birth_date", func(t *testing.T, val string) {
+				_, err := time.Parse(time.RFC3339, val)
+				assert.NoError(t, err, "value %q should be RFC3339 timestamp", val)
+			}},
+			{"company", func(t *testing.T, val string) {
+				assert.NotEmpty(t, val)
+			}},
+			{"job_title", func(t *testing.T, val string) {
+				assert.NotEmpty(t, val)
+			}},
+			{"currency", func(t *testing.T, val string) {
+				assert.Len(t, val, 3)
+			}},
+			{"lang", func(t *testing.T, val string) {
+				assert.Len(t, val, 2)
+			}},
+			{"locale", func(t *testing.T, val string) {
+				assert.Contains(t, val, "-")
+			}},
+		}
+
+		for _, tc := range tests {
+			t.Run(tc.name, func(t *testing.T) {
+				fd := &mockFieldDescriptor{
+					name: protoreflect.Name(tc.name),
+					kind: protoreflect.StringKind,
+				}
+				s := fauxrpc.String(fd, opts)
+				tc.validate(t, s)
+			})
+		}
 	})
 }
